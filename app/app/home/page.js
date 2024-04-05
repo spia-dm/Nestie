654f4221e9267015f8277f7f65792e5068ad52e3
sidxt <srisiddhaarthp@gmail.com>
2024-04-06 00:11:23 +0530

    test

--- app/app/home/page.js
+++ app/app/home/page.js
@@ -1,17 +1,24 @@
 import Link from 'next/link';
+
 const Home = () => {
   return (
     <div className="bg-[#B5C0D0] h-screen flex flex-col">
-      <nav className="bg-[#3E3232] h-16 flex justify-end items-center px-4">
-        <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
-          Log In
-        </Link>
-        <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
-          Get Started
-        </Link>
-      </nav>
-      <div>
-        <h2 className="font-josefin_slab text-center text-5xl text-[#B5C0D0]">PropertEase</h2>
+      <div className="bg-[#3E3232] min-h-16 max-h-16">
+        <div className="flex justify-center flex-grow">
+          <h2 className="font-josefin_slab text-5xl text-[#B5C0D0] underline text-outline-black2 mt-2">PropertEase</h2>
+        </div>
+        <div className="flex flex-row justify-end -mt-11 mb-2 mr-2">
+          <div>
+            <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
+              Log In
+            </Link>
+          </div>
+          <div>
+            <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
+              Get Started
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/app/page.js
+++ app/app/page.js
@@ -2,14 +2,23 @@ import Link from 'next/link';
 const Landing = () => {
   return (
     <div className="bg-[#B5C0D0] h-screen flex flex-col">
-      <nav className="bg-[#3E3232] h-16 flex justify-end items-center px-4">
-        <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
-          Log In
-        </Link>
-        <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
-          Get Started
-        </Link>
-      </nav>
+      <div className="bg-[#3E3232] min-h-16 max-h-16">
+        <div className="flex justify-center flex-grow">
+          <h2 className="font-josefin_slab text-5xl text-[#B5C0D0] underline text-outline-black2 mt-14"></h2>
+        </div>
+        <div className="flex flex-row justify-end -mt-11 mb-2 mr-2">
+          <div>
+            <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
+              Log In
+            </Link>
+          </div>
+          <div>
+            <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-col justify-center items-center flex-1">
         <h1 className="text-[#625757] text-outline-black text-7xl bg-[#CCD3CA] font-josefin_slab p-2 mb-8">
           PropertEase
