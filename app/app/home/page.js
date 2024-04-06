0f614823f4c61f88fc8a6705d4c8e78d617e050a
sidxt <srisiddhaarthp@gmail.com>
2024-04-07 01:17:13 +0530

    test

--- app/app/home/page.js
+++ app/app/home/page.js
@@ -1,6 +1,16 @@
-import Link from 'next/link';
-
+"use client"
+import { signIn, signOut, useSession } from 'next-auth/react'
+import {redirect} from 'next/navigation'
 const Home = () => {
+  const session =useSession();
+  console.log(session);
+  if(session.status ==="loading"){
+    return <p>Loading....</p>
+  }
+  if(session.status ==="unauthenticated"){
+      redirect('/')
+  }
+
   return (
     <div className="bg-[#B5C0D0] h-screen flex flex-col">
       <div className="bg-[#3E3232] min-h-16 max-h-16">
@@ -8,16 +18,6 @@ const Home = () => {
           <h2 className="font-josefin_slab text-5xl text-[#B5C0D0] underline text-outline-black2 mt-2">PropertEase</h2>
         </div>
         <div className="flex flex-row justify-end -mt-11 mb-2 mr-2">
-          <div>
-            <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
-              Log In
-            </Link>
-          </div>
-          <div>
-            <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
-              Get Started
-            </Link>
-          </div>
         </div>
       </div>
     </div>
--- app/app/page.js
+++ app/app/page.js
@@ -1,21 +1,30 @@
-import Link from 'next/link';
+"use client"
+import { signIn, signOut, useSession } from 'next-auth/react'
+import {redirect} from 'next/navigation'
 const Landing = () => {
+
+
+    const session =useSession();
+    console.log(session);
+    if(session.status ==="loading"){
+        return <p>Loading....</p>
+    }
   return (
     <div className="bg-[#B5C0D0] h-screen flex flex-col">
       <div className="bg-[#3E3232] min-h-16 max-h-16">
         <div className="flex justify-center flex-grow">
           <h2 className="font-josefin_slab text-5xl text-[#B5C0D0] underline text-outline-black2 mt-14"></h2>
         </div>
-        <div className="flex flex-row justify-end -mt-11 mb-2 mr-2">
+        <div className="flex flex-row justify-end -mt-[3%] mr-2">
           <div>
-            <Link href="/signin" className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
+            <button onClick={()=>signIn("google")} className="rounded-3xl text-3xl font-josefin_slab bg-[#625757] pl-3 p-2 pr-3 mr-2 text-[#CCD3CA] font-bold transition-all duration-300 ease-in-out active:bg-opacity-20">
               Log In
-            </Link>
+            </button>
           </div>
           <div>
-            <Link href="/signup" className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
+            <button onClick={()=>signIn("google")} className="rounded-3xl text-3xl font-josefin_slab bg-[#B5C0D0] pl-3 p-2 pr-3 text-[#625757] font-bold active:bg-[#9CAFAA] hover:text-outline-black transition-all duration-300 ease-in-out">
               Get Started
-            </Link>
+            </button>
           </div>
         </div>
       </div>
@@ -26,9 +35,9 @@ const Landing = () => {
         <p className="text-[#625757] text-3xl font-josefin_slab mb-8 text-center font-extrabold">
           Unlock Your Dream Home, One Tap Away.
         </p>
-        <Link href="/signup" className="text-[#625757] bg-[#CCD3CA] p-2 active:bg-[#9CAFAA] hover:text-outline-black text-3xl font-josefin_slab font-extrabold transition-all duration-300 ease-in-out rounded-2xl border-[#625757] border-2">
+        <button onClick={()=>signIn("google")} className="text-[#625757] bg-[#CCD3CA] p-2 active:bg-[#9CAFAA] hover:text-outline-black text-3xl font-josefin_slab font-extrabold transition-all duration-300 ease-in-out rounded-2xl border-[#625757] border-2">
           Get Started
-        </Link>
+        </button>
       </div>
     </div>
   );
