651ff9152f6c838317255fb7261910ea0f723a56
sidxt <srisiddhaarthp@gmail.com>
2024-04-08 03:38:34 +0530

    test

---
+++ app/app/utils/mongoose.tsx
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+import { connect, Connection } from 'mongoose';
+
+interface ConnectionState {
+ isConnected: boolean;
+}
+const conn: ConnectionState = {
+ isConnected: false,
+};
+
+export async function dbConnect(): Promise<void> {
+ if (conn.isConnected) {
+   return;
+ }
+
+ try {
+   const db = await connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nextjs');
+   conn.isConnected = true;
+
+   console.log('Mongodb connected to db');
+ } catch (err) {
+   console.error('Mongodb Error:', err.message);
+ }
+}
+
+const connection: Connection = mongoose.connection;
+
+connection.on('connected', () => console.log('Mongodb connected to db'));
+
+connection.on('error', (err: any) => console.error('Mongodb Error:', err.message));
